fbd23cb5afc2e649977fdbd5391c5e301389a66d
Nils Hjalmarson <nils.hjalmarson@gmail.com>
2024-11-19 22:11:12 +0100

    feat: Update project configuration and improve random number generation

--- C13/random.cs
+++ C13/random.cs
@@ -2,11 +2,13 @@ namespace C13;
 
 public class CommonFunctions
 {
+
+    private static readonly Lock padlock = new();
     private static readonly Random getrandom = new Random();
 
     public static int GetRandomNumber(int min, int max)
     {
-        lock (getrandom)
+        lock (padlock)
         {
             return getrandom.Next(min, max);
         }
